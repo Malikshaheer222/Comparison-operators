103dd253ed03c85665474976cd73ecf73de9f0e9
Shaheer Malik <139039230+Malikshaheer222@users.noreply.github.com>
2023-07-27 08:36:29 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+// There are 7 comparison operators
+// examples
+let n = 10;
+console.log(n == 10);
+console.log(n === 8);
+console.log(n > 7);
+console.log(n < 7);
+console.log(n != 10);
+console.log(n <= 7);
+console.log(n >= 7);
---
+++ index.ts
@@ -0,0 +1,12 @@
+// There are 7 comparison operators
+
+         // examples
+
+let n=10;
+console.log(n==10);
+console.log(n===8);
+console.log(n>7);
+console.log(n<7);
+console.log(n !=10);
+console.log(n<=7);
+console.log(n>=7);
